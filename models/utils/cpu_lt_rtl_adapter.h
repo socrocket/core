9ddcda997b6a80cddc8ff9c508be04ebe892b83f
Thomas Schuster <schuster@c3e.cs.tu-bs.de>
2011-10-05 07:38:27 +0000

    Save last local changes before delete
    git-svn-id: https://ntserv1.ida.ing.tu-bs.de/svn/hwswcosim/trunk@518 d83f2ebd-4275-1f45-b601-53736ebb63cd

--- models/utils/cpu_lt_rtl_adapter.h
+++ models/utils/cpu_lt_rtl_adapter.h
@@ -1,128 +1,108 @@
 
-#ifndef __CPU_LT_RTL_ADAPTER_H__
-#define __CPU_LT_RTL_ADAPTER_H__
+#ifndef __TLMCPU_RTLCACHE_TRANSACTOR_H__
+#define __TLMCPU_RTLCACHE_TRANSACTOR_H__
 
 #include <tlm.h>
 #include <simple_target_socket.h>
 
-#include "stdio.h"
 #include "verbose.h"
+#include "amba.h"
 #include "mmu_cache_wrapper.h"
 
 #include "../lib/icio_payload_extension.h"
 #include "../lib/dcio_payload_extension.h"
 
-
-class cpu_lt_rtl_adapter : public sc_module {
+/// Adapter class for connecting TLM Leon simulator (or testbench) to RTL version of mmu_cache
+class tlmcpu_rtlcache_transactor : public sc_module {
 
  public:
 
-  // clock and reset inputs
+  // Clock and reset inputs
   sc_in<bool> rst;
   sc_in_clk clk;
 
-  // instruction ports to/from mmu_cache
+  // Instruction ports to/from mmu_cache
   sc_out<icache_in_type> ici;
   sc_in<icache_out_type> ico;
 
-  // data ports to/from mmu_cache
+  // Data ports to/from mmu_cache
   sc_out<dcache_in_type> dci;
   sc_in<dcache_out_type> dco;
 
-  // cpu instruction cache socket
-  tlm_utils::simple_target_socket<cpu_lt_rtl_adapter> icio;
+  // CPU instruction cache socket
+  tlm_utils::simple_target_socket<tlmcpu_rtlcache_transactor> icio;
   
-  // cpu data cache socket
-  tlm_utils::simple_target_socket<cpu_lt_rtl_adapter> dcio;
+  // CPU data cache socket
+  tlm_utils::simple_target_socket<tlmcpu_rtlcache_transactor> dcio;
 
-  // member functions
+  // Member functions
   // ----------------
  
-  // forward transport function for icio socket
-  void icio_custom_b_transport(tlm::tlm_generic_payload &payload, sc_core::sc_time &delay);
+  // Blocking transport function for icio socket
+  void icio_b_transport(tlm::tlm_generic_payload &payload, sc_core::sc_time &delay);
 
-  // forward transport function for dcio socket
-  void dcio_custom_b_transport(tlm::tlm_generic_payload &payload, sc_core::sc_time &delay);
+  // Non-blocking forward transport function for icio socket
+  tlm::tlm_sync_enum icio_nb_transport_fw(tlm::tlm_generic_payload &payload, sc_core::sc_time &delay);
 
-  // data to signal functions
-  void iread(unsigned int address, unsigned int * instr, unsigned int flush);
-  void dread(unsigned int address, unsigned int * data, unsigned int length, unsigned int asi, unsigned int flush, unsigned int flushl, unsigned int lock);
-  void dwrite(unsigned int address, unsigned int * data, unsigned int length, unsigned int asi, unsigned int flush, unsigned int flushl, unsigned int lock);
+  // Blocking transport function for dcio socket
+  void dcio_b_transport(tlm::tlm_generic_payload &payload, tlm::tlm_phase &phase, sc_core::sc_time &delay);
 
+  // Non-blocking forward transport function for dcio socket
+  tlm::tlm_sync_enum dcio_nb_transport_fw(tlm::tlm_generic_payload &payload, sc_core::sc_time &delay);
+
+  // Threads for processing icio transactions
+  void instruction_processor();
+  void instruction_post_processor();
+
+  // Threads for processing dcio transactions
+  void data_processor();
+  void data_post_processor();
+
+  // Mux dci and ici input signals
+  void dci_signal_mux();
+  void ici_signal_mux();
+
+  // Called at start of simulation for signal initialization
   void start_of_simulation();
 
-  // latch output of caches
-  void data_latch();
-
-  // state machine threads
-  void fsm_clock_tick();
-  void fsm_next_state();
-
   /// Constructor
-  SC_HAS_PROCESS(cpu_lt_rtl_adapter);
-  cpu_lt_rtl_adapter(sc_core::sc_module_name name) :
-                                       sc_module(name),
-				       rst("rst"),
-                                       clk("clk"),
-                                       ici("ici"),
-				       ico("ico"),
-				       dci("dci"),
-				       dco("dco"),
-				       icio("icio"),
-				       dcio("dcio"),
-                                       ival("ival"),
-                                       dval("dval")
-    {
-
-    // register forward transport functions for icio and dcio sockets
-    icio.register_b_transport(this, &cpu_lt_rtl_adapter::icio_custom_b_transport);
-    dcio.register_b_transport(this, &cpu_lt_rtl_adapter::dcio_custom_b_transport);
-
-    // 3x threads for state machine implementation
-    // ===========================================
-    
-    // 1. state transition at clock tick
-    SC_THREAD(fsm_clock_tick);
-    sensitive << clk.pos();
-
-    // 2. assign signals and determine next state (async)
-    SC_THREAD(fsm_next_state);
-    sensitive << state << iread_pending << dread_pending << dwrite_pending << ico << dco << ival << dval;
-
-  }
+  SC_HAS_PROCESS(tlmcpu_rtlcache_transactor);
+  tlmcpu_rtlcache_transactor(sc_core::sc_module_name name, amba::amba_layer_ids abstractionLayer);
 
  private:
 
-  enum adapter_state { idle, ireadaddr, dreadaddr, dwriteaddr, dwritemiss, ireadmiss, dreadmiss };
+  // Signals for ici port connections
+  sc_signal<sc_lv<32> >  i_rpc;
+  sc_signal<sc_lv<32> >  i_fpc;
+  sc_signal<sc_lv<32> >  i_dpc;
+  sc_signal<sc_logic>    i_rbranch;
+  sc_signal<sc_logic>    i_fbranch;
+  sc_signal<sc_logic>    i_inull;
+  sc_signal<sc_logic>    i_su;
+  sc_signal<sc_logic>    i_flush;
+  sc_signal<sc_logic>    i_flushl;
+  sc_signal<sc_lv<29> >  i_fline;
+  sc_signal<sc_logic>    i_pnull; 
 
-  sc_signal<adapter_state> state;
-  sc_signal<adapter_state> nextstate;
-
-  sc_signal<icache_in_type> ival;
-  sc_signal<dcache_in_type> dval;
-
-  sc_signal<sc_logic> ico_mds;
-  sc_signal<sc_logic> dco_mds;
-  sc_signal<sc_logic> ico_hold;
-  sc_signal<sc_logic> dco_hold;
-
-  sc_signal<bool> dwrite_pending;
-  sc_signal<bool> dread_pending;
-  sc_signal<bool> iread_pending;
-
-  sc_event dwrite_done;
-  sc_event dread_done;
-  sc_event iread_done;
-
-  unsigned int hdl_data;
-  unsigned int hdl_instr;
-
-  // buffer cache outputs (data and instruction) @clock
-  unsigned int ico_data_reg;
-  unsigned int dco_data_reg;
-
-  unsigned int transactor_state;
+  // Signals for dci port connections
+  sc_signal<sc_lv<8>     d_asi;
+  sc_signal<sc_lv<32> >  d_maddress;
+  sc_signal<sc_lv<32> >  d_eaddress;
+  sc_signal<sc_lv<32> >  d_edata;
+  sc_signal<sc_lv<2> >   d_size;
+  sc_signal<sc_logic>    d_enaddr;
+  sc_signal<sc_logic>    d_eenaddr;
+  sc_signal<sc_logic>    d_nullify;
+  sc_signal<sc_logic>    d_lock;
+  sc_signal<sc_logic>    d_read;
+  sc_signal<sc_logic>    d_write;
+  sc_signal<sc_logic>    d_flush;
+  sc_signal<sc_logic>    d_flushl;
+  sc_signal<sc_logic>    d_dsuen;
+  sc_signal<sc_logic>    d_msu;
+  sc_signal<sc_logic>    d_esu;
+  sc_signal<sc_logic>    d_intack;  
   
 };  
 
-#endif // _CPU_LT_RTL_ADAPTER_H__
+#endif // _TLMCPU_RTLCACHE_ADAPTER_H__
