40c12d0b1c6fd67c616dd2f04bf10284ab440879
Rolf Meyer <meyer@c3e.cs.tu-bs.de>
2011-12-07 13:38:01 +0000

    Initial commit of AHBMem. The module is a branch of the utils/tb_ahb_mem class.
    git-svn-id: https://ntserv1.ida.ing.tu-bs.de/svn/hwswcosim/trunk@564 d83f2ebd-4275-1f45-b601-53736ebb63cd

---
+++ models/ahbmem/ahbmem.cpp
@@ -0,0 +1,291 @@
+// *****************************************************************************
+// Copyright 2010, Institute of Computer and Network Engineering,
+//                 TU-Braunschweig
+// All rights reserved
+// Any reproduction, use, distribution or disclosure of this program,
+// without the express, prior written consent of the authors is 
+// strictly prohibited.
+//
+// University of Technology Braunschweig
+// Institute of Computer and Network Engineering
+// Hans-Sommer-Str. 66
+// 38118 Braunschweig, Germany
+//
+// ESA SPECIAL LICENSE
+//
+// This program may be freely used, copied, modified, and redistributed
+// by the European Space Agency for the Agency's own requirements.
+//
+// The program is provided "as is", there is no warranty that
+// the program is correct or suitable for any purpose,
+// neither implicit nor explicit. The program and the information in it
+// contained do not necessarily reflect the policy of the 
+// European Space Agency or of TU-Braunschweig.
+// *****************************************************************************
+// Title:      ahbmem.cpp
+//
+// ScssId:
+//
+// Origin:     HW-SW SystemC Co-Simulation SoC Validation Platform
+//
+// Purpose:    Provide a test bench memory class with AHB slave interface.
+//
+// Method:     Memory is modeled with a map.
+//             DMI and streaming width fields are ignored.
+//             Delays are not modeled.
+//             Address checking is performed in that way, that transactions
+//             are only executed if the slave select condition defined in
+//             grlib user manual holds.
+//             Transactions generating a correct slave select but exceeding
+//             the memory region due to their length are reported as warning
+//             and executed anyhow.
+//
+// Modified on $Date: 2011-08-18 15:18:53 +0200 (Thu, 18 Aug 2011) $
+//          at $Revision: 492 $
+//          by $Author: HWSWSIM $
+//
+// Principal:  European Space Agency
+// Author:     VLSI working group @ IDA @ TUBS
+// Maintainer: Rolf Meyer
+// Reviewed:
+// *****************************************************************************
+
+#include "ahbmem.h"
+#include <fstream>
+#include <iostream>
+#include "verbose.h"
+
+/// Constructor
+AHBMem::AHBMem(const sc_core::sc_module_name nm, // Module name
+               uint16_t haddr_, // AMBA AHB address (12 bit)
+               uint16_t hmask_, // AMBA AHB address mask (12 bit)
+               amba::amba_layer_ids ambaLayer, // abstraction layer
+               uint32_t slave_id) :
+            sc_module(nm),
+            AHBDevice(slave_id, /*Gaisler*/ 0x01, /*AHBMem*/0x00E, 0, 0, BAR(AHBDevice::AHBMEM, hmask_, 0, 0, haddr_)),
+            ahb("ahb", amba::amba_AHB, ambaLayer, false /* arbiter? */),
+            mTransactionPEQ("TransactionPEQ"),
+            ahbBaseAddress(static_cast<uint32_t> (hmask_ & haddr_) << 20),
+            ahbSize(~(static_cast<uint32_t> (hmask_) << 20) + 1), hmask(
+                    static_cast<uint32_t> (hmask_ << 20)), haddr(
+                    static_cast<uint32_t> (haddr_ << 20)),
+            mhaddr(haddr_),
+            mhmask(hmask_) {
+
+    // haddr and hmask must be 12 bit
+    assert(!((mhaddr|mhmask)>>12));
+
+    // Display AHB slave information
+    v::info << name() << "********************************************************************" << v::endl;
+    v::info << name() << "* Create AHB simulation memory with following parameters:           " << v::endl;
+    v::info << name() << "* haddr/hmask: " << hex << mhaddr << "/" << mhmask                    << v::endl;
+    v::info << name() << "* Slave base address: " << hex << get_base_addr()                     << v::endl;
+    v::info << name() << "* Slave size (bytes): " << hex << get_size()                          << v::endl;
+    v::info << name() << "********************************************************************" << v::endl;
+
+    // For LT register blocking transport
+    if(ambaLayer==amba::amba_LT) {
+      ahb.register_b_transport(this, &AHBMem::b_transport);
+    }
+
+    // For AT register non-blocking transport
+    if(ambaLayer==amba::amba_AT) {
+      ahb.register_nb_transport_fw(this, &AHBMem::nb_transport_fw);
+    }
+
+    ahb.register_transport_dbg(this, &AHBMem::transport_dbg);
+
+    SC_THREAD(processTXN);
+}
+
+void AHBMem::dorst() {
+    mem.clear();
+}
+
+/// Destructor
+AHBMem::~AHBMem() {
+    // Delete memory contents
+    mem.clear();
+} 
+
+/// TLM blocking transport function
+void AHBMem::b_transport(tlm::tlm_generic_payload &trans, sc_core::sc_time &delay) {
+  // Is the address for me
+  if(!((mhaddr ^ (trans.get_address() >> 20)) & mhmask)) {
+    // warn if access exceeds slave memory region
+    if((trans.get_address() + trans.get_data_length()) >
+      (ahbBaseAddress + ahbSize)) {
+       v::warn << name() << "Transaction exceeds slave memory region" << endl;
+    }
+
+    // consume router delay (address phase)
+    wait(delay);
+
+    // reset timing
+    delay = sc_core::SC_ZERO_TIME;
+
+    if(trans.get_command() == tlm::TLM_WRITE_COMMAND) {
+      for(uint32_t i = 0; i < trans.get_data_length(); i++) {
+	    
+	v::debug << name() << "Write with address: " << hex << trans.get_address() + i << " and data: " << hex << (unsigned int)*(trans.get_data_ptr() + i) << v::endl;
+
+        // write simulation memory
+        mem[trans.get_address() + i] = *(trans.get_data_ptr() + i);
+      }
+
+      delay = clock_cycle * (trans.get_data_length() >> 2);
+      trans.set_response_status(tlm::TLM_OK_RESPONSE);
+    } else {
+      for(uint32_t i = 0; i < trans.get_data_length(); i++) {
+       
+	v::debug << name() << "Read with address: " << hex << trans.get_address() + i << " to return: " << hex << (unsigned int)mem[trans.get_address() + i] << v::endl;
+
+	// read simulation memory
+	*(trans.get_data_ptr() + i) = mem[trans.get_address() + i];
+        
+      }
+
+      delay = clock_cycle * (trans.get_data_length() >> 2);
+      trans.set_response_status(tlm::TLM_OK_RESPONSE);
+
+    }
+
+  // address not valid
+  } else {
+
+    v::error << name() << "Address not within permissable slave memory space" << v::endl;
+    trans.set_response_status(tlm::TLM_ADDRESS_ERROR_RESPONSE);
+
+  }
+}
+
+tlm::tlm_sync_enum AHBMem::nb_transport_fw(tlm::tlm_generic_payload& trans, tlm::tlm_phase& phase, sc_core::sc_time& delay) {
+
+  v::debug << name() << "nb_transport_fw received transaction " << hex << &trans << " with phase: " << phase << v::endl;
+
+  // The master has sent BEGIN_REQ
+  if (phase == tlm::BEGIN_REQ) {
+
+    // Writes have to wait for BEGIN_DATA
+    if (trans.get_command() == tlm::TLM_READ_COMMAND) {
+      mTransactionPEQ.notify(trans,delay);
+    }
+
+    phase = tlm::END_REQ;
+    delay = SC_ZERO_TIME;
+    return(tlm::TLM_UPDATED);
+
+  } else if (phase == amba::BEGIN_DATA) {
+    mTransactionPEQ.notify(trans,delay);
+    
+  } else if (phase == tlm::END_RESP) {
+    // nothing to do
+  } else {
+
+    v::error << name() << "Invalid phase in call to nb_transport_fw!" << v::endl;
+    trans.set_response_status(tlm::TLM_COMMAND_ERROR_RESPONSE);
+  }
+
+  return(tlm::TLM_ACCEPTED);
+}
+
+void AHBMem::processTXN() {
+
+  tlm::tlm_phase phase;
+  sc_core::sc_time delay;
+  tlm::tlm_sync_enum status;
+
+  tlm::tlm_generic_payload * trans;
+
+  while(1) {
+    wait(mTransactionPEQ.get_event());
+    while((trans = mTransactionPEQ.get_next_transaction())) {
+      v::debug << name() << "Process transaction " << hex << trans << v::endl;
+      if(trans->get_command() == tlm::TLM_WRITE_COMMAND) {
+        for(uint32_t i = 0; i < trans->get_data_length(); i++) {
+	  v::debug << name() << "Write with address: " << hex << trans->get_address() + i << " and data: " << hex << (unsigned int)*(trans->get_data_ptr() + i) << v::endl;
+
+	  // write simulation memory
+          mem[trans->get_address() + i] = *(trans->get_data_ptr() + i);
+
+        }
+
+        // Send END_DATA
+        phase = amba::END_DATA;
+        delay = clock_cycle * (trans->get_data_length() >> 2);
+
+	v::debug << name() << "Transaction " << hex << trans << " call to nb_transport_bw with phase " << phase << " (delay: " << delay << ")" << v::endl;
+
+        trans->set_response_status(tlm::TLM_OK_RESPONSE);
+        status = ahb->nb_transport_bw(*trans, phase, delay);
+	assert((status==tlm::TLM_ACCEPTED)||(status==tlm::TLM_COMPLETED));
+      } else {
+        for(uint32_t i = 0; i < trans->get_data_length(); i++) {
+	  v::debug << name() << "Read with address: " << hex << trans->get_address() + i << " to return: " << hex << (unsigned int)mem[trans->get_address() + i] << v::endl;
+
+	  // read simulation memory
+	  *(trans->get_data_ptr() + i) = mem[trans->get_address() + i];
+        
+        }
+
+        // Send BEGIN_RESP
+        phase = tlm::BEGIN_RESP;
+        delay = clock_cycle * (trans->get_data_length() >> 2);
+
+	v::debug << name() << "Transaction " << hex << trans << " call to nb_transport_bw with phase " << phase << " (delay: " << delay << ")" << v::endl; 
+
+        trans->set_response_status(tlm::TLM_OK_RESPONSE);
+
+        status = ahb->nb_transport_bw(*trans, phase, delay);
+
+	assert(status==tlm::TLM_ACCEPTED);
+      }
+    }
+  }
+}
+
+void AHBMem::writeByteDBG(const uint32_t address, const uint8_t byte) {
+   mem[address] = byte;
+}
+
+// TLM debug interface
+unsigned int AHBMem::transport_dbg(tlm::tlm_generic_payload &gp) {
+    // Check address befor before doing anything else
+    if(!((haddr ^ (gp.get_address() & 0xfff00000)) & hmask)) {
+        // warn if access exceeds slave memory region
+        if((gp.get_address() + gp.get_data_length()) >
+           (ahbBaseAddress + ahbSize)) {
+            v::warn << name() << "Transaction exceeds slave memory region"
+                    << endl;
+        }
+
+        switch(gp.get_command()) {
+            // Read command
+            case tlm::TLM_READ_COMMAND:
+                for(uint32_t i = 0; i < gp.get_data_length(); i++) {
+                    *(gp.get_data_ptr() + i) = mem[gp.get_address() + i];
+                }
+                gp.set_response_status(tlm::TLM_OK_RESPONSE);
+                return gp.get_data_length();
+                break;
+
+                // Write command
+            case tlm::TLM_WRITE_COMMAND:
+                for(uint32_t i = 0; i < gp.get_data_length(); i++) {
+                    mem[gp.get_address() + i] = *(gp.get_data_ptr() + i);
+                }
+                gp.set_response_status(tlm::TLM_OK_RESPONSE);
+                return gp.get_data_length();
+                break;
+
+            // Neither read nor write command
+            default:
+                v::warn << name() << "Received unknown command." << endl;
+                gp.set_response_status(tlm::TLM_COMMAND_ERROR_RESPONSE);
+                return 0;
+                break;
+        }
+    } // if( !((haddr ^ (gp.get_address() & 0xfff00000)) & hmask) )
+
+    return 0;
+}
---
+++ models/ahbmem/ahbmem.h
@@ -0,0 +1,124 @@
+// *****************************************************************************
+// Copyright 2010, Institute of Computer and Network Engineering,
+//                 TU-Braunschweig
+// All rights reserved
+// Any reproduction, use, distribution or disclosure of this program,
+// without the express, prior written consent of the authors is 
+// strictly prohibited.
+//
+// University of Technology Braunschweig
+// Institute of Computer and Network Engineering
+// Hans-Sommer-Str. 66
+// 38118 Braunschweig, Germany
+//
+// ESA SPECIAL LICENSE
+//
+// This program may be freely used, copied, modified, and redistributed
+// by the European Space Agency for the Agency's own requirements.
+//
+// The program is provided "as is", there is no warranty that
+// the program is correct or suitable for any purpose,
+// neither implicit nor explicit. The program and the information in it
+// contained do not necessarily reflect the policy of the 
+// European Space Agency or of TU-Braunschweig.
+// *****************************************************************************
+// Title:      ahbmem.h
+//
+// ScssId:
+//
+// Origin:     HW-SW SystemC Co-Simulation SoC Validation Platform
+//
+// Purpose:    Provide a test bench memory class with AHB slave interface.
+//
+// Modified on $Date: 2011-08-18 15:18:53 +0200 (Thu, 18 Aug 2011) $
+//          at $Revision: 492 $
+//          by $Author: HWSWSIM $
+//
+// Principal:  European Space Agency
+// Author:     VLSI working group @ IDA @ TUBS
+// Maintainer: Rolf Meyer
+// Reviewed:
+// *****************************************************************************
+
+#ifndef AHBMEM_H
+#define AHBMEM_H
+
+#include <systemc.h>
+#include <tlm.h>
+#include <map>
+#include <amba.h>
+
+#if defined(MTI_SYSTEMC)
+#include "peq_with_get.h"
+#else
+#include "tlm_utils/peq_with_get.h"
+#endif
+
+#include "ahbdevice.h"
+#include "clkdevice.h"
+
+class AHBMem : public sc_module, public AHBDevice, public CLKDevice {
+    public:
+        SC_HAS_PROCESS(AHBMem);
+        
+        /// Constructor
+        /// @brief Constructor for the test bench memory class
+        /// @param haddr AHB address of the AHB slave socket (12 bit)
+        /// @param hmask AHB address mask (12 bit)
+        /// @param ambaLayer Abstraction layer used (AT/LT)
+        /// @param infile File name of a text file to initialize the memory from
+        /// @param addr Start address for memory initilization
+        AHBMem(const sc_core::sc_module_name nm, uint16_t haddr_, uint16_t hmask_ = 0, amba::amba_layer_ids ambaLayer = amba::amba_LT, uint32_t slave_id = 0);
+
+        /// Destructor
+        ~AHBMem();
+
+        /// AMBA slave socket
+        amba::amba_slave_socket<32> ahb;
+
+        /// TLM blocking transport function
+        void b_transport(tlm::tlm_generic_payload &trans, sc_core::sc_time &delay);
+
+        /// TLM non blocking transport function
+        tlm::tlm_sync_enum nb_transport_fw(tlm::tlm_generic_payload& trans, tlm::tlm_phase& phase, sc_core::sc_time& delay);
+
+        /// TLM debug interface
+        unsigned int transport_dbg(tlm::tlm_generic_payload& gp);
+        
+        /// Reset callback
+        void dorst();
+
+        /// @brief Delete memory content
+        void clear_mem() {
+            mem.clear();
+        }
+
+        /// @brief Method to write a byte into the memory
+        /// @param addr Write address
+        /// @param byte Write data
+        void writeByteDBG(const uint32_t addr, const uint8_t byte);
+
+    private:
+        /// The actual memory
+        std::map<uint32_t, uint8_t> mem;
+        
+        /// Thread processign transactions when they emerge from the PEQ
+        void processTXN();
+
+        /// Payload event queue. Transactions accompanied with a non-zero
+        /// delay argument are queued here in case of AT abstraction level.
+        tlm_utils::peq_with_get<tlm::tlm_generic_payload> mTransactionPEQ;
+
+        /// AHB slave base address and size
+        const uint32_t ahbBaseAddress;
+        // size is saved in bytes
+        const uint32_t ahbSize;
+        const uint32_t hmask;
+        const uint32_t haddr;
+
+        /// 12 bit MSB address and mask (constructor parameters)
+        const uint32_t mhaddr;
+        const uint32_t mhmask;
+};
+
+#endif
