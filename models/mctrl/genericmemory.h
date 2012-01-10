5558b5ec073e2ea30c5c9ddc1ef5cf777b966d4e
Rolf Meyer <meyer@c3e.cs.tu-bs.de>
2012-01-10 15:05:06 +0000

    GenericMemory - comments, pow_mon included, calc_delay removed
    git-svn-id: https://ntserv1.ida.ing.tu-bs.de/svn/hwswcosim/trunk@646 d83f2ebd-4275-1f45-b601-53736ebb63cd

--- models/mctrl/genericmemory.h
+++ models/mctrl/genericmemory.h
@@ -22,14 +22,14 @@
 // contained do not necessarily reflect the policy of the 
 // European Space Agency or of TU-Braunschweig.
 //*********************************************************************
-// Title:      Generic_memory.h
+// Title:      genericmemory.h
 //
 // ScssId:
 //
 // Origin:     HW-SW SystemC Co-Simulation SoC Validation Platform
 //
-// Purpose:    memory module modeling all types of memory supported by
-//             mctrl: RAM, std I/O, SRAM, SDRAM
+// Purpose:    Generic memory model to be used with the SoCRocket
+//             MCTRL. Can be configured as ROM, IO, SRAM or SDRAM.
 //
 // Modified on $Date: 2011-05-09 20:31:53 +0200 (Mon, 09 May 2011) $
 //          at $Revision: 416 $
@@ -53,28 +53,55 @@
 #include "memdevice.h"
 #include "ext_erase.h"
 
-class GenericMemory : public MEMDevice, public sc_core::sc_module {
+/// @brief This class models a generic memory. Depending on the configuration
+/// it can be used as ROM, IO, SRAM or SDRAM, in conjunction with the SoCRocket MCTRL.
+class GenericMemory : public MEMDevice, 
+	              public sc_core::sc_module {
+
     public:
-        /// Slave socket: communication to the Mctrl
+
+        /// Slave socket -  for communication with Mctrl
         gs::socket::target_socket<32> bus;
 
-        GenericMemory(sc_module_name name, MEMDevice::device_type type, uint32_t banks, uint32_t bsize, uint32_t bits, uint32_t cols);
+	/// Creates a new Instance of GenericMemory
+	///
+	/// @param name The SystemC name of the component to be created
+	/// @param type The type of memory to be modeled (0-ROM, 1-IO, 2-SRAM, 3-SDRAM)
+	/// @param banks Number of parallel banks
+	/// @param bsize Size of one memory bank in bytes (all banks always considered to have equal size)
+	/// @param bits Bit width of memory
+	/// @param cols Number of SDRAM cols.
+        GenericMemory(sc_module_name name, 
+		      MEMDevice::device_type type, 
+		      uint32_t banks, 
+		      uint32_t bsize, 
+		      uint32_t bits, 
+		      uint32_t cols
+		      bool pow_mon);
+
+	/// Destructor
         ~GenericMemory();
         
         typedef std::map<uint32_t, uint8_t> type;
         type memory;
 
+	/// TLM 2.0 blocking transport function
         void b_transport(tlm::tlm_generic_payload& gp, sc_time& delay);
 
+	/// TLM 2.0 debug transport function
         unsigned int transport_dbg(tlm::tlm_generic_payload& gp);
         
+	/// Read byte from functional memory
         uint8_t read(const uint32_t addr);
+
+	/// Write byte to functional memory
         void write(const uint32_t addr, const uint8_t byte);
 
-        //erase sdram required for deep power down and PASR mode
+        /// Erase sdram - Required for deep power down and PASR mode
         void erase(uint32_t start, uint32_t end);
 
-        sc_core::sc_time calc_delay(bool write, uint32_t size, bool at, bool erase = false) const;
+	/// Power monitoring
+	bool g_pow_mon;
 };
 
 #endif
