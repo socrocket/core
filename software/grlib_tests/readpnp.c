d25ef22863aecc2627bd040e8332f3c6c5c1137e
Rolf Meyer <meyer@c3e.cs.tu-bs.de>
2012-01-19 13:54:17 +0000

    Adding pnp tests
    git-svn-id: https://ntserv1.ida.ing.tu-bs.de/svn/hwswcosim/trunk@681 d83f2ebd-4275-1f45-b601-53736ebb63cd

--- software/grlib_tests/readpnp.c
+++ software/grlib_tests/readpnp.c
@@ -5,9 +5,9 @@ typedef unsigned int uint32_t;
 typedef unsigned char uint8_t;
 
 int main(int argc, char *argv[]) {
-  uint32_t *ahb_pnp_master = (uint32_t *)0xFF000000;
-  uint32_t *ahb_pnp_slave = (uint32_t *)0xFF000000;
-  uint32_t *apb_pnp_slave = (uint32_t *)0x800FF000;
+  uint32_t *ahb_pnp_master = (uint32_t *)0xFFFFF000;
+  uint32_t *ahb_pnp_slave =  (uint32_t *)0xFFFFF800;
+  uint32_t *apb_pnp_slave =  (uint32_t *)0x800FF000;
 
   // AHB Master 0 MMUCache
   // AHB Slave 0 MCtrl
@@ -17,8 +17,21 @@ int main(int argc, char *argv[]) {
   uint8_t *ahbmem = (uint8_t *)0xA0000000;
   
   uint32_t mctrl_mcfg1 = *((uint32_t *)0x80000000);
-  printf("1. Read MCFG1: 0x%08x\n", mctrl_mcfg1);
+  printf("To use this test you are adviced to use the standard singlecore lt platform\n");
+  printf("This test reads and writes all memmorys. Uses MCtrl, AHBMem and PNP area over AHB & APB\n");
+  printf("\n");
   
+  printf("1. Read MCFG1 MCtrl Register from APB:\n");
+  printf("   Reading: 0x%08x\n", mctrl_mcfg1);
+  if(mctrl_mcfg1 == 0x10280033) {
+    printf("   Test successful\n");
+  } else {
+    printf("   Test failed\n");
+  }
+  printf("\n");
+  
+  printf("2. DBG Read from MCtrl\n");
+  printf("   Preparing memory (Write Bytes)\n");
   mctrl_sdram[0] = 'R';
   mctrl_sdram[1] = 'e';
   mctrl_sdram[2] = 'a';
@@ -34,33 +47,62 @@ int main(int argc, char *argv[]) {
   mctrl_sdram[12] = 'R';
   mctrl_sdram[13] = 'A';
   mctrl_sdram[14] = 'M';
-  mctrl_sdram[15] = '\n';
-  mctrl_sdram[16] = '\0';
+  mctrl_sdram[15] = '\0';
+  printf("   Reading:   '%s'\n", (char *)mctrl_sdram);
+  printf("   Should be: 'Read from SDRAM'\n");
+  printf("\n");
   
+  printf("3. DBG Read from AHBMem\n");
+  printf("   Preparing memory (Write Bytes)\n");
   ahbmem[0] = 'R';
-  ahbmem[0] = 'e';
-  ahbmem[0] = 'a';
-  ahbmem[0] = 'd';
-  ahbmem[0] = ' ';
-  ahbmem[0] = 'f';
-  ahbmem[0] = 'r';
-  ahbmem[0] = 'o';
-  ahbmem[0] = 'm';
-  ahbmem[0] = ' ';
-  ahbmem[0] = 'A';
-  ahbmem[0] = 'H';
-  ahbmem[0] = 'B';
-  ahbmem[0] = 'M';
-  ahbmem[0] = 'e';
-  ahbmem[0] = 'm';
-  ahbmem[0] = '\n';
-  ahbmem[0] = '\0';
+  ahbmem[1] = 'e';
+  ahbmem[2] = 'a';
+  ahbmem[3] = 'd';
+  ahbmem[4] = ' ';
+  ahbmem[5] = 'f';
+  ahbmem[6] = 'r';
+  ahbmem[7] = 'o';
+  ahbmem[8] = 'm';
+  ahbmem[9] = ' ';
+  ahbmem[10] = 'A';
+  ahbmem[11] = 'H';
+  ahbmem[12] = 'B';
+  ahbmem[13] = 'M';
+  ahbmem[14] = 'e';
+  ahbmem[15] = 'm';
+  ahbmem[16] = '\0';
+  printf("   Read:      '%s'\n", (char *)ahbmem);
+  printf("   Should be: 'Read from AHBMem'\n");
+  printf("\n");
   
-  printf("2. Test MCtrl DBG Read: %s\n", (char *)mctrl_sdram);
-  printf("3. Test APBMem DBG Read: %s\n", (char *)ahbmem);
-  printf("4. Test AHB Master PNP Read 0x%08hx\n", ahb_pnp_master[0]);
-  printf("4. Test AHB Slave PNP Read 0x%08x\n", ahb_pnp_slave[0]);
-  printf("4. Test APB Slave PNP Read 0x%08x\n", apb_pnp_slave[0]);
+  
+  printf("4. AHB Master PNP Read\n");
+  printf("   Reading: 0x%08hx\n", ahb_pnp_master[0]);
+  if(ahb_pnp_master[0] == 0x01003000) {
+    printf("   Test successful\n");
+  } else {
+    printf("   Test failed\n");
+  }
+  printf("\n");
+  
+  printf("5. AHB Slave PNP Read\n");
+  printf("   Reading: 0x%08hx\n", ahb_pnp_slave[0]);
+  if(ahb_pnp_slave[0] == 0x0400F000) {
+    printf("   Test successful\n");
+  } else {
+    printf("   Test failed\n");
+  }
+  printf("\n");
+  
+  printf("6. APB Slave PNP Read\n");
+  printf("   Reading: 0x%08hx\n", apb_pnp_slave[0]);
+  if(apb_pnp_slave[0] == 0x0400F000) {
+    printf("   Test successful\n");
+  } else {
+    printf("   Test failed\n");
+  }
+  printf("\n");
+  
   // Try to read from all
   // Try to write a text in the AHBMem/MCtrl
   // Printf the text from AHBMem/MCtrl
