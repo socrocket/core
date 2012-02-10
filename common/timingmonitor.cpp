6005bae5eb96c6fbefea739c7fda1ce4af4c0e46
Thomas Schuster <schuster@c3e.cs.tu-bs.de>
2012-02-10 19:03:15 +0000

    The timeing monitor is printing tables now
    git-svn-id: https://ntserv1.ida.ing.tu-bs.de/svn/hwswcosim/trunk@766 d83f2ebd-4275-1f45-b601-53736ebb63cd

--- common/timingmonitor.cpp
+++ common/timingmonitor.cpp
@@ -166,6 +166,7 @@ void TimingMonitor::report_timing() {
   v::report << "TimingMonitor" << "******************************************************************" << v::endl;
   v::report << "TimingMonitor" << "* TIMING SUMMARY " << v::endl;
   v::report << "TimingMonitor" << "* -------------- " << v::endl;
+  v::report << "TimingMonitor" << " Phase, Name, SystemC Time, SystemC Start, SystemC End, Real Time (sec)" << v::endl;
 
   // Walk through timing map
   while(it != timing_map.end()) {
@@ -173,12 +174,13 @@ void TimingMonitor::report_timing() {
     id  = it->first;
     tmp = it->second;
 
-    v::report << "TimingMonitor" << "* Phase: " << id << v::endl;
-    v::report << "TimingMonitor" << "* Name: " << ((it->second).name) << v::endl;
-    v::report << "TimingMonitor" << "* SystemC Time: " << TimingMonitor::phase_systime(id) \
+    v::report << "TimingMonitor" << id << ", " << ((it->second).name) << ", " << TimingMonitor::phase_systime(id) << ", " << tmp.st_start << ", " << tmp.st_end << ", " << TimingMonitor::phase_realtime(id) << v::endl;
+    //v::report << "TimingMonitor" << "* Phase: " << id << v::endl;
+    //v::report << "TimingMonitor" << "* Name: " << ((it->second).name) << v::endl;
+    //v::report << "TimingMonitor" << "* SystemC Time: " << TimingMonitor::phase_systime(id) \
 	    << " (Start: " << tmp.st_start << " End: " << tmp.st_end << ")" << v::endl;
-    v::report << "TimingMonitor" << "* Real Time: " << TimingMonitor::phase_realtime(id) << " sec " << v::endl;
-    v::report << "TimingMonitor" << "* --------------------------------------------------------------" << v::endl;
+    //v::report << "TimingMonitor" << "* Real Time: " << TimingMonitor::phase_realtime(id) << " sec " << v::endl;
+    //v::report << "TimingMonitor" << "* --------------------------------------------------------------" << v::endl;
   
     it++;
   }
