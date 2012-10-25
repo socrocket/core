196a353b4bea546efe8751f466350a3890e7981c
Rolf Meyer <meyer@c3e.cs.tu-bs.de>
2012-10-25 14:33:08 +0200

    Implementing Instructions after configuration generation

--- tools/generator/wizard/generate.py
+++ tools/generator/wizard/generate.py
@@ -11,61 +11,21 @@ class GeneratePage(QtGui.QWizardPage):
         #self.setPixmap(QtGui.QWizard.WatermarkPixmap,
         #        QtGui.QPixmap(':/images/watermark1.png'))
 
-        self.label1 = QtGui.QLabel("Progress ...")
-        self.progress = QtGui.QProgressBar()
-        self.label2 = QtGui.QLabel("Details:")
-        self.list = QtGui.QListWidget()
+        self.label2 = QtGui.QLabel("Instructions:")
+        self.text = QtGui.QTextBrowser()
         self.templates = templates
         layout = QtGui.QVBoxLayout()
-        layout.addWidget(self.label1)
-        layout.addWidget(self.progress)
         layout.addWidget(self.label2)
-        layout.addWidget(self.list)
+        layout.addWidget(self.text)
         self.setLayout(layout)
         self.ready = False
-        self.progress.setValue(0)
     
     def isComplete(self):
         return self.ready
 
     def initializePage(self):
-        self.list.clear()
+        self.text.setText(self.templates.getTemplate().instructions % (self.templates.getTemplate().getConfigurationFile()))
         self.ready = False
-        self.progress.setValue(0)
         self.templates.generate()
-        """
-        # gather informations
-        gen = self.template.template.generators()
-        files = self.template.template.files()
-        steps = 2 + len(files) + len(gen)
-        self.progress.setValue(1 * 100 / steps)
-        # mkdir
-        path = os.path.join("platforms", '-'.join((str(self.template.template.base), str(self.name.conf.conf.conf)))) 
-        if os.path.isdir(path):
-            shutil.rmtree(path)
-        os.makedirs(path)
-        self.progress.setValue(2 * 100 / steps)
-        # writing files
-        num = 2
-        for f in files:
-            fpath = os.path.join(path, f.type, f.name)
-            if not os.path.isdir(os.path.join(path, f.type)):
-                os.makedirs(os.path.join(path, f.type))
-            file = open(fpath, "w")
-            file.write(f.data)
-            num += 1
-            self.progress.setValue(num * 100 / steps)
-        
-        # generating files
-        for f in gen:
-            fpath = os.path.join(path, f.type, f.name)
-            if not os.path.isdir(os.path.join(path, f.type)):
-                os.makedirs(os.path.join(path, f.type))
-            file = open(fpath, "w")
-            file.write(f.generate(self.conf.model))
-            num += 1
-            self.progress.setValue(num * 100 / steps)
-        """
-        self.progress.setValue(100)
         self.ready = True
         self.completeChanged.emit()
