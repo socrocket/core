4704c22ce3a955d24492b527c1e5b3b812b053a6
Timo Veit <t.veit@tu-bs.de>
2013-06-18 08:01:30 +0200

    Moved content of gs_config.cpp to gs_config.h. Deleted gs_config.cpp

--- common/gs_config/gs_config.h
+++ common/gs_config/gs_config.h
@@ -7,18 +7,6 @@ namespace gs {
         class gs_config_base
         {
         public:  
-            void setProperty(std::string key, std::string value);
-            std::string getProperty(std::string key);
-            vmap<std::string, std::string> getProperties();
-            void deleteProperty(std::string key);
-            bool exists(std::string key);
-				easy_init addProperties();
-        private:
-            vmap<std::string, std::string> m_properties;
-        };
-    }
-}
-
 class easy_init
 {
 public:
@@ -39,3 +27,57 @@ public:
 private:
     gs::cnf::gs_config_base* m_owner;
 };
+
+            void gs_config_base::setProperty(std::string key, std::string value)
+            {
+                m_properties[key] = value;
+            }
+
+            std::string gs_config_base::getProperty(std::string key)
+            {
+                vmap<std::string, std::string>::iterator it;
+                it = m_properties.find(key);
+                if (it != m_properties.end())
+                {
+                    return it->second;
+                }
+                else return "Property not found";
+            }
+
+            vmap<std::string, std::string> gs_config_base::getProperties()
+            {
+                vmap<std::string, std::string>::iterator it;
+                vmap<std::string, std::string> properties;
+                for (it = m_properties.begin(); it != m_properties.end(); ++it)
+                {
+                    properties[it->first] = it->second;
+                }
+                return properties;
+            }
+
+            void gs_config_base::deleteProperty(std::string key)
+            {
+                vmap<std::string, std::string>::iterator it;
+                it = m_properties.find(key);
+                m_properties.erase(it);
+            }
+
+            bool gs_config_base::exists(std::string key)
+            {
+                vmap<std::string, std::string>::iterator it;
+                it = m_properties.find(key);
+                if (it != m_properties.end())
+                {
+                    return true;
+                }
+                else return false;
+            }
+            easy_init gs_config_base::addProperties()            
+            {
+                return easy_init(this);
+            }
+        private:
+            vmap<std::string, std::string> m_properties;
+        };
+    }
+}
