9cf3156d55934741f0237e70cb7a1371dad449ff
Rolf Meyer <meyer@c3e.cs.tu-bs.de>
2013-12-23 22:52:33 +0100

    Introducing PySc -- Python SystemC Bridge. This is inspired by GreenScript but is mutch lighter and will develop in another direction.

---
+++ common/sc_api.h
@@ -0,0 +1,37 @@
+// Detect SystemC API and define the macro SYSTEMC_API
+// (include detection for other SystemC vendors if necessary)
+
+#ifndef SYSTEMC_API_H
+#define SYSTEMC_API_H
+
+#include <systemc>
+
+#ifdef SYSTEMC_API
+#  if SYSTEMC_API != 210 && SYSTEMC_API != 220 && SYSTEMC_API != 230
+#    error The SYSTEMC_API macro is set to an unknown value. Accepted values are 210 or 220. Please, see the source code gs_sc_api_detection.h for more information.
+#  endif
+#else
+// OSCI SystemC 2.1
+#  if SYSTEMC_VERSION == 20050714
+#    define SYSTEMC_API 210
+#  endif
+// OSCI SystemC 2.2.0 and 2.2.05jun06
+#  if SYSTEMC_VERSION == 20070314 || SYSTEMC_VERSION == 20060505
+#    define SYSTEMC_API 220
+#  endif
+// OSCI SystemC 2.3.0
+#  if SYSTEMC_VERSION == 20120701
+#    define SYSTEMC_API 230
+#  endif
+// SystemC API to use must be defined at this point
+// include the detection above for other SystemC vendors to
+// define the SYSTEMC_API macro
+#  ifndef SYSTEMC_API
+#    error The SYSTEMC_API macro is not set and could not be detected. Please, see the source code gs_sc_api_detection.h for more information.
+#  endif
+#endif
+
+// Extend SystemC with our custom functions.
+#include "sc_find.h"
+
+#endif
