1dfc87a65c82fd213a0c170d1f9b8d6785c96749
Rolf Meyer <meyer@c3e.cs.tu-bs.de>
2014-09-01 22:45:11 +0200

    Adding vector<string> gs_config for osemu cli arguments

--- common/gs_config/gs_config_native.hpp
+++ common/gs_config/gs_config_native.hpp
@@ -665,6 +665,148 @@ inline std::string operator+ (const char *lhs, const gs_config<std::string>& rhs
     return std::string(lhs) + const_cast<gs_config<std::string>& >(rhs).getValue();
 }
 
+///////////////////////////////////////////////////////////////////////////////// //
+///////////////////// gs_param< std::vector<std::string> > ////////////////////// //
+///////////////////////////////////////////////////////////////////////////////// //
+
+/// Template specialization for gs_param< std::vector<std::string> >.
+/**
+ * Default value = empty vector.
+ */
+// TODO: maybe template<typename T>
+// class gs_param< std::vector<T> >
+// issue: (de)serialization of T
+template<>
+class gs_config<std::vector<std::string> >
+  : public gs_config_t<std::vector<std::string> > {
+  /// Typedef for the value.
+  typedef std::vector<std::string> val_type;
+  public:
+    GS_CONFIG_HEAD;
+
+    /////////////////////////
+    // additional functions
+
+    bool empty() const {return my_value.empty(); }
+    // TODO optional: More of the vector functions
+
+    /////////////////////////
+    // overloaded functions
+
+    /// Overloads gs_param_base::getTypeString
+    const std::string getTypeString() const {
+      return std::string("std::vector<std::string>");
+    }
+
+    /// Overloads gs_param_base::getType
+    const Param_type getType() const {
+      return PARTYPE_NOT_AVAILABLE;
+    }
+
+    const std::string& operator [] (const int index) { 
+      return my_value[index];
+    }
+
+    size_t size() {
+      my_value.size();
+    }
+
+    /// Overloads gs_param_t<T>::serialize
+    /**
+     * Serializes the vector to a single line comma separated list
+     * of the members.<br>
+     * Members surrounded by quotes (").
+     * The whole string surrounded by { }
+     */
+    std::string convertValueToString(const val_type &val) const {
+      return static_convertValueToString(val);
+    }
+
+    inline static std::string static_convertValueToString(const val_type &val) {
+      std::ostringstream ss;
+      ss << "{";
+      bool first = true;
+      val_type::const_iterator iter;
+
+      for (iter = val.begin(); iter != val.end(); iter++) {
+        if (!first) {ss << ','; }
+        first = false;
+        // make sure included "s are handled
+
+        std::string::size_type loc = iter->find('"');
+        std::string aux = *iter;
+
+        while (loc != std::string::npos) {
+          if (aux[loc - 1] != '\\') {
+            aux = aux.substr(0, loc) + "\\" + aux.substr(loc, aux.npos);
+          }
+          loc = aux.find('"', loc + 1);
+        }
+
+        ss << '"' << aux << '"'; // not nescessarily quotes
+      }
+      ss << "}";
+      return ss.str();
+    }
+
+    /// Static convertion function called by virtual deserialize and others (e.g. GCnf_API)
+    /**
+     * Deserializes a single-line string into a vector.
+     * - The string has to look like: "{"value1","value 2","value 3"}"
+     * - Optionally there may be spaces between the values and the comma.
+     * - so far the value must not contain quotes: TODO!!
+     */
+    inline static bool static_deserialize(val_type &target_val, const std::string &str) {
+      std::istringstream ss(str);
+
+      GS_PARAM_DUMP_WITHNAME("gs_param", "deserialize: string='" << ss.str().c_str() << "'");
+      if (ss.str().length() == 0) {
+        target_val.clear();
+        GS_PARAM_DUMP_WITHNAME("gs_param", "vector<string> deserialize: set default value");
+        return true;
+      } else {
+        target_val.clear();
+
+        std::string::size_type index = str.find("{");
+        if (index == str.npos) {
+          SC_REPORT_ERROR("gs_param", "vector<string> deserialize error!");
+        }
+        std::string my_string = str;
+        std::string value;
+        std::string word;
+        std::string::size_type indexB, indexC;
+        do {
+          if (my_string == "}") {break; }
+
+          index = 0;
+          do {
+            index = my_string.find("\"", index);
+          } while (my_string[index - 1] == '\\' && index != std::string::npos);
+
+          indexB = index;
+          do {
+            indexB = my_string.find("\"", indexB + 1);
+          } while (my_string[indexB - 1] == '\\' && indexB != std::string::npos);
+
+          if ((index == std::string::npos) || (indexB == std::string::npos)) {break; }
+
+          value = my_string.substr(index + 1, indexB - index - 1);
+
+          // now we must handle \" that can be inside the string, and transform them to single "
+          indexC = value.find("\\\"");
+          while (indexC != std::string::npos) {
+            value = value.substr(0, indexC) + value.substr(indexC + 1, value.npos);
+            indexC = value.find("\\\"", indexC);
+          }
+
+          target_val.push_back(value);
+          my_string = my_string.substr(indexB + 1, word.npos);
+        } while (index != std::string::npos);
+        return true;
+      }
+      return true;
+    }
+};
 
 // /////////////////////////////////////////////////////////////////////////////// //
 // /////////////////// gs_config< UNSIGNED LONG LONG > //////////////////////////// //
