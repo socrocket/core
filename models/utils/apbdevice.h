1bd9969431fe72ca865d793335ef057160e232b3
Rolf Meyer <rolf.meyer@durania.net>
2014-10-30 23:05:51 +0100

    Adding AHB/APBDeviceBase in extra Files for Python

--- models/utils/apbdevice.h
+++ models/utils/apbdevice.h
@@ -17,62 +17,7 @@
 #ifndef MODELS_UTILS_APBDEVICE_H_
 #define MODELS_UTILS_APBDEVICE_H_
 
-#include "core/common/amba.h"
-#include <stdint.h>
-#include "core/common/base.h"
-#include "core/common/amba.h"
-
-/// This class is a base class for grlib models. It implements the device plug and play informations.
-/// Together with the APBBridge class it implements the plug and play feature of the grlib.
-/// @see APBCtrl
-class APBDeviceBase {
-  public:
-    /// Empty destructor
-    virtual ~APBDeviceBase() {}
-
-    /// Returns the device id.
-    virtual uint16_t get_apb_device_id() const = 0;
-
-    /// Returns the vendor id.
-    virtual uint8_t get_apb_vendor_id() const = 0;
-
-    /// Returns the device register file.
-    /// A set of 8 registers as specified by the grlib manual.
-    /// See section: 14.2.2 (Page 79)
-    virtual const uint32_t *get_apb_device_info() = 0;
-
-    /// Returns the device type.
-    /// Should be APBIO ;-)
-    virtual const AMBADeviceType get_apb_type() const = 0;
-
-    /// Returns the Bus specific most significant 12bit of the base address
-    /// Shifted to the lowest bits in the word.
-    virtual uint32_t get_apb_base() const = 0;
-
-    /// Returns the Bus specific mask of the most significant 12bit of the address
-    /// Shifted to the lowest bits in the word.
-    virtual uint32_t get_apb_mask() const = 0;
-
-    /// Returns the Bus specific base address of the device.
-    /// @see get_bar_addr
-    /// @return The device base address.
-    virtual sc_dt::uint64 get_apb_base_addr() = 0;
-    virtual uint32_t get_apb_base_addr_() const = 0;
-
-    /// Returns the size of the hole device as seen from the bus.
-    /// @see get_bar_size
-    /// @return The device size.
-    virtual sc_dt::uint64 get_apb_size() = 0;
-    virtual uint32_t get_apb_size_() const = 0;
-
-    virtual uint32_t get_apb_relative_addr(uint32_t addr) const = 0;
-
-    /// Returns the bus id of the module (pindex)
-    virtual uint32_t get_apb_pindex() const = 0 ;
-
-    /// Prints the device info of the device.
-    virtual void print_apb_device_info(char *name) const = 0;
-};
+#include "core/models/utils/apbdevicebase.h"
 
 /// This class is a base class for grlib models. It implements the device plug and play informations.
 /// Together with the APBBridge class it implements the plug and play feature of the grlib.
