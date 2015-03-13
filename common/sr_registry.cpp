72e88bb2981f6d28cf3ac42d62c14778da550f93
Rolf Meyer <rolf.meyer@durania.net>
2015-03-13 16:13:04 +0100

    Multiregistry

--- common/sr_registry.cpp
+++ common/sr_registry.cpp
@@ -11,24 +11,34 @@
 /// @author Rolf Meyer
 #include "core/common/sr_registry.h"
 
-SrModuleRegistry::map_t SrModuleRegistry::reg = SrModuleRegistry::map_t();
+SrModuleRegistry::map_map_t SrModuleRegistry::m_members = SrModuleRegistry::map_map_t();
 
-SrModuleRegistry::SrModuleRegistry(std::string type, SrModuleRegistry::generator_f funct, std::string file) : m_funct(funct), m_file(file) {
-  SrModuleRegistry::reg.insert(std::make_pair(type, this));
+SrModuleRegistry::SrModuleRegistry(std::string group, std::string type, SrModuleRegistry::generator_f funct, std::string file) : 
+  m_funct(funct), m_file(file) {
+  SrModuleRegistry::map_map_t::iterator iter = SrModuleRegistry::m_members.find(group);
+  if(iter == SrModuleRegistry::m_members.end()) {
+    m_members.insert(std::make_pair(group, SrModuleRegistry::map_t()));
+    iter = SrModuleRegistry::m_members.find(group);
+  }
+  iter->second.insert(std::make_pair(type, this));
 };
 
-sc_core::sc_object *SrModuleRegistry::create_object_by_name(std::string type, std::string name) {
-  SrModuleRegistry::map_t::iterator iter = SrModuleRegistry::reg.find(type);
-  if(iter != SrModuleRegistry::reg.end()) {
-    return iter->second->m_funct(name.c_str());
+sc_core::sc_object *SrModuleRegistry::create_object_by_name(std::string group, std::string type, std::string name) {
+  SrModuleRegistry::map_map_t::iterator iter = SrModuleRegistry::m_members.find(group);
+  if(iter != SrModuleRegistry::m_members.end()) {
+    SrModuleRegistry::map_t::iterator item = iter->second.find(type);
+    if(item != iter->second.end()) {
+      return item->second->m_funct(name.c_str());
+    }
   }
   return NULL;
 }
 
-std::set<std::string> SrModuleRegistry::get_module_files() {
+std::set<std::string> SrModuleRegistry::get_module_files(std::string group) {
   std::set<std::string> result;
-  for(SrModuleRegistry::map_t::iterator iter = SrModuleRegistry::reg.begin(); iter != SrModuleRegistry::reg.end(); ++iter) {
-    result.insert(iter->second->m_file);
+  SrModuleRegistry::map_map_t::iterator iter = SrModuleRegistry::m_members.find(group);
+  for(SrModuleRegistry::map_t::iterator item = iter->second.begin(); item != iter->second.end(); ++item) {
+    result.insert(item->second->m_file);
   }
   return result;
 }
