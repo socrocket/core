9d321d8807eacfaf6cfb9a642422a1f1703bcd75
Rolf Meyer <rolf.meyer@durania.net>
2015-05-29 19:22:37 +0200

    Improving sr_registry to make includes

--- core/common/sr_registry.h
+++ core/common/sr_registry.h
@@ -23,6 +23,7 @@ class SrModuleRegistry {
     typedef std::map<std::string, map_t> map_map_t;
     typedef sc_core::sc_object *(*generator_f)(sc_core::sc_module_name);
     SrModuleRegistry(std::string group, std::string type, generator_f funct, std::string file);
+    void included();
     static sc_core::sc_object *create_object_by_name(std::string group, std::string type, std::string name);
     static std::set<std::string> get_module_files(std::string group);
 
@@ -36,15 +37,20 @@ class SrModuleRegistry {
 
 #define \
   SR_HAS_MODULE_GENERATOR(type, funct) \
-  static SrModuleRegistry __sr_module_registry_##funct##__("module", type, &funct, __FILE__); \
-  volatile SrModuleRegistry *__sr_module_registry_##funct = &__sr_module_registry_##funct##__;
+  static SrModuleRegistry __sr_module_registry_##funct##__("module", #type, &funct, __FILE__); \
+  volatile SrModuleRegistry *__sr_module_registry_##type = &__sr_module_registry_##funct##__;
 
 #define \
   SR_HAS_MODULE(type) \
     sc_core::sc_object *create_##type(sc_core::sc_module_name mn) { \
       return new type(mn); \
     } \
-    SR_HAS_MODULE_GENERATOR(#type, create_##type);
+    SR_HAS_MODULE_GENERATOR(type, create_##type);
+
+#define \
+  SR_INCLUDE_MODULE(type) \
+  extern SrModuleRegistry *__sr_module_registry_##type; \
+  (__sr_module_registry_##type)->included();
 
 #endif  // COMMON_SR_REGISTRY_H_
 /// @}
