162a48c53fed136a73dfed51a7e7856873cea23a
P. Siegl <patrick.siegl@gmx.de>
2015-08-24 13:59:57 +0200

    fixed two testcases

--- core/software/trapgen/crc.c
+++ core/software/trapgen/crc.c
@@ -17,10 +17,9 @@ unsigned short icrc1(unsigned short crc, unsigned char onech)
     unsigned short ans=(crc^onech << 8);
 
     for (i=0;i<8;i++) {
+        ans <<= 1;
         if (ans & 0x8000)
-            ans = (ans <<= 1) ^ 4129;
-        else
-            ans <<= 1;
+            ans ^= 4129;
     }
     return ans;
 }
