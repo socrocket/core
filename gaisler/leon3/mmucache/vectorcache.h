ae709ba47a4b5d3f93d291c2f35064d9aa784f35
P. Siegl <patrick.siegl@gmx.de>
2015-11-19 16:24:35 +0100

    [vectorcache] applied Lillian's nice formatted header

--- gaisler/leon3/mmucache/vectorcache.h
+++ gaisler/leon3/mmucache/vectorcache.h
@@ -3,13 +3,13 @@
 /// @{
 /// @file vectorcache.h
 /// Class definition of a virtual cache model. The cache can be configured
-/// direct mapped or set associative. Set-size, line-size and replacement
+/// direct mapped or set associative. Way-size, line-size and replacement
 /// strategy can be defined through constructor arguments.
 ///
 /// @date 2010-2015
 /// @copyright All rights reserved.
 ///            Any reproduction, use, distribution or disclosure of this
-///            program, without the express, prior written consent of the 
+///            program, without the express, prior written consent of the
 ///            authors is strictly prohibited.
 /// @author Thomas Schuster
 ///
@@ -35,10 +35,11 @@
 /// @brief virtual cache model, contain common functionality of instruction and data cache
 class vectorcache : public DefaultBase, public cache_if {
 
- public:
+  /// --------------------------------------------------------------------------
+  /// @name Interface Data Methods
+  /// @{
 
-  // Memory interface functions (mem_if)
-  // -----------------------------------------------------------
+public:
   /// Read from cache
   virtual bool mem_read(unsigned int address, unsigned int asi, unsigned char * data,
                         unsigned int len, sc_core::sc_time * t,
@@ -47,8 +48,14 @@ class vectorcache : public DefaultBase, public cache_if {
   virtual void mem_write(unsigned int address, unsigned int asi, unsigned char * data,
                          unsigned int len, sc_core::sc_time * t,
                          unsigned int * debug, bool is_dbg, bool &cacheable, bool is_lock);
-  /// Flush cache
-  virtual void flush(sc_core::sc_time * t, unsigned int * debug, bool is_dbg);
+
+  /// @} Interface Data Methods
+  /// --------------------------------------------------------------------------
+  /// @name Interface Control Methods
+  /// @{
+
+  /// Read cache configuration register (ASI 0x2)
+  virtual unsigned int read_config_reg(sc_core::sc_time *t);
 
   /// Read data cache tags (ASI 0xe)
   virtual void read_cache_tag(unsigned int address, unsigned int * data,
@@ -62,121 +69,125 @@ class vectorcache : public DefaultBase, public cache_if {
   /// Write data cache entries/data (ASI 0xf)
   virtual void write_cache_entry(unsigned int address, unsigned int * data,
                                  sc_core::sc_time *t);
-  /// Read cache configuration register (ASI 0x2)
-  virtual unsigned int read_config_reg(sc_core::sc_time *t);
+  /// Flush cache
+  virtual void flush(sc_core::sc_time * t, unsigned int * debug, bool is_dbg);
 
+  /// Snooping function (invalidates cache line(s))
+  virtual void snoop_invalidate(const t_snoop& snoop, const sc_core::sc_time& delay);
+
+  /// @} Interface Control Methods
+  /// --------------------------------------------------------------------------
+  /// @name Internal Methods
+  /// @{
+protected:
+  /// Address parsing one-liners
+  inline unsigned get_tag(unsigned address)
+    {return (address >> (m_idx_bits + m_offset_bits));}
+  inline unsigned get_idx(unsigned address)
+    {return ((address << m_tag_bits) >> (m_tag_bits + m_offset_bits));}
+  inline unsigned get_offset(unsigned address)
+    {return ((address << (m_tag_bits + m_idx_bits)) >> (m_tag_bits + m_idx_bits));}
+  inline unsigned get_address(unsigned tag, unsigned idx, unsigned offset)
+    {return (tag << (m_idx_bits + m_offset_bits)) | (idx << m_offset_bits) | offset;}
+
+  /// Transforms a cache-line offset into a valid mask
+  inline unsigned int offset2valid(unsigned int offset, unsigned int len = 4);
+
+  /// Returns number of the way to be refilled - depending on replacement strategy
+  unsigned int replacement_selector(unsigned int idx, unsigned int mode);
+
+  /// Updates the lru counters for every cache hit
+  void lru_update(unsigned int idx, unsigned int set_select);
+
+  /// Updates the lrr bits for every line replacement
+  void lrr_update(unsigned int idx, unsigned int set_select);
+
+  /// Reads a cache line from a given cache way.
+  /// Returns an iterator to found line (more useful than pointer for looping through ways of a given index).
+  /// Returns null for incorrect parameters.
+  inline std::vector<t_cache_line>::iterator lookup_line(unsigned idx, unsigned way) {return cache_mem->begin()+(idx*(m_sets+1)+way);}
+
+  /// Searches for a cache tag in all cache ways. Updates power information for reading tags.
+  /// Returns found way if tag matches and data is valid, otherwise -1.
+  int locate_line(unsigned const tag, unsigned const idx, unsigned const offset, unsigned const len,
+                  sc_core::sc_time * delay);
+
+  /// Allocates a cache line in a given way. Updates timing and power information.
+  /// Returns the allocated way (should be same as parameter), otherwise -1.
+  int update_line(unsigned const tag, unsigned const idx, unsigned const offset,
+                  unsigned const way, unsigned const len,
+                  unsigned char* const data,
+                  sc_core::sc_time * delay, unsigned* debug, bool& cacheable, bool is_dbg);
+
+  /// Allocates a cache line in either an invalid way or a way found be replacement selection.
+  /// Updates timing and power information.
+  /// Returns the allocated way, otherwise -1.
+  int allocate_line(unsigned const tag, unsigned const idx, unsigned const offset,
+                    unsigned const len,
+                    unsigned char* const data,
+                    sc_core::sc_time * delay, unsigned* debug, bool& cacheable, bool is_dbg);
+
+  /// @} Internal Methods
+  /// --------------------------------------------------------------------------
+  /// @name Diagnostic Methods
+  /// @{
+
+public:
   /// Returns the mode bits of the cache
   virtual unsigned int check_mode()=0;
 
   /// Returns type of cache implementing this interface
   virtual t_cache_type get_cache_type()=0;
 
-  /// Snooping function (invalidates cache line(s))
-  virtual void snoop_invalidate(const t_snoop& snoop, const sc_core::sc_time& delay);
-
-  // Debug and helper functions
-  // --------------------------
   /// Display of cache lines for debug
   virtual void dbg_out(unsigned int line);
 
   /// Helper functions for definition of clock cycle
   void clkcng(sc_core::sc_time &clk);
 
-  /// Transforms a cache-line offset into a valid mask
-  inline unsigned int offset2valid(unsigned int offset, unsigned int len = 4);
+  /// @} Diagnostic Methods
+  /// --------------------------------------------------------------------------
+  /// @name Constructors and Destructors
+  /// @{
 
-  void end_of_simulation();
-
- private:
-
-  // internal behavioral functions
-  // -----------------------------
-  /// reads a cache line from a cache set
-  inline std::vector<t_cache_line>::iterator lookup_line( unsigned int idx, unsigned int way ) {
-    return (cache_mem->begin() + (idx * (m_sets + 1) + way));
-  }
-  /// returns number of the set to be refilled - depending on replacement strategy
-  unsigned int replacement_selector(unsigned int idx, unsigned int mode);
-  /// updates the lru counters for every cache hit
-  void lru_update(unsigned int idx, unsigned int way_select);
-  /// updates the lrr bits for every line replacement
-  void lrr_update(unsigned int idx, unsigned int way_select);
-
-  inline unsigned get_tag( unsigned address ) {
-    return (address >> (m_idx_bits + m_offset_bits));
-  }
-
-  inline unsigned get_idx( unsigned address ) {
-    return (address << (32 - (m_idx_bits + m_offset_bits))) >> (32 - m_idx_bits);
-  }
-
-  inline unsigned get_offset( unsigned address ) {
-    return ((address << (32 - m_offset_bits)) >> (32 - m_offset_bits));
-  }
-
-  int locate_line(unsigned int const tag,
-                             unsigned int const idx,
-                             unsigned int const offset,
-                             unsigned int const len,
-                             sc_core::sc_time * delay);
-
-  int allocate_line(unsigned const tag,
-                                unsigned const idx,
-                                unsigned const offset,
-                                unsigned const len,
-                                unsigned char* const data,
-                                sc_core::sc_time * delay, unsigned int * debug, bool& cacheable, bool is_dbg);
-
-  int update_line(unsigned const tag,
-                              unsigned const idx,
-                              unsigned const offset,
-                              unsigned const way,
-                              unsigned const len,
-                              unsigned char* const data,
-                              sc_core::sc_time * delay,
-                              unsigned int * debug,
-                              bool& cacheable, bool is_dbg);
-
- protected:
+protected:
   // constructor
-  // args: sysc module name, pointer to AHB read/write methods (of parent), delay on read hit, delay on read miss (incr), number of sets, setsize in kb, linesize in b, replacement strategy
+  // args: sysc module name, pointer to AHB read/write methods (of parent), delay on read hit, delay on read miss (incr), number of ways, waysize in kb, linesize in b, replacement strategy
   /// @brief Constructor of data cache
-  /// @param name                              SystemC module name
-  /// @param _mmu_cache                        Pointer to top-level class of cache subsystem (mmu_cache) for access to AHB bus interface
-  /// @param _tlb_adaptor                      Pointer to memory management unit
-  /// @param burst_en                          Allows the cache to be switched in burst fetch mode (by CCR setting)
-  /// @param sets                              Number of cache sets
-  /// @param setsize                           Size of a cache set (in kbytes)
-  /// @param linesize                          Size of a cache line (in bytes)
-  /// @param repl                              Cache replacement strategy
-  /// @param lram                              Local RAM configured
-  /// @param lramstart                         The 8 MSBs of the local ram start address (16MB segment)
-  /// @param lramsize                          Size of local ram (size in kbyte = 2^lramsize)
-  /// @param pow_mon                           Enables power monitoring
-  vectorcache(ModuleName name, mmu_cache_if * _mmu_cache,
-              mem_if * _tlb_adaptor, unsigned int mmu_en,
-              unsigned int burst_en,
-	      bool new_linefetch_en,
-              unsigned int sets,
-              unsigned int setsize, unsigned int setlock,
-              unsigned int linesize, unsigned int repl,
-              unsigned int lram, unsigned int lramstart,
-              unsigned int lramsize,
-              bool pow_mon);
+  vectorcache(ModuleName name,              ///> SystemC module name
+              mmu_cache_if* _mmu_cache,     ///> Pointer to top-level class of cache subsystem (mmu_cache) for access to AHB bus interface
+              mem_if* _tlb_adaptor,         ///> Pointer to memory management unit
+              unsigned int mmu_en,          ///> Memory management unit enabled
+              unsigned int burst_en,        ///> Allows the cache to be switched in burst fetch mode (by CCR setting)
+              bool new_linefetch_en,        ///>
+              unsigned int sets,            ///> Number of cache ways
+              unsigned int setsize,         ///> Size of a cache way (in kbytes)
+              unsigned int setlock,         ///>
+              unsigned int linesize,        ///> Size of a cache line (in words)
+              unsigned int repl,            ///> Cache replacement strategy
+              unsigned int lram,            ///> Local RAM configured
+              unsigned int lramstart,       ///> The 8 MSBs of the local ram start address (16MB segment)
+              unsigned int lramsize,        ///> Size of local ram (size in kbyte = 2^lramsize)
+              bool pow_mon);                ///> Enables power monitoring
 
   virtual ~vectorcache();
 
- protected:
+public:
+  void end_of_simulation();
 
-  // data members
-  // ------------
-  /// pointer to the class with the amba interface
+  /// @} Constructors and Destructors
+  /// --------------------------------------------------------------------------
+  /// @name Data
+  /// @{
+
+protected:
+  /// Pointer to the class with the amba interface
   mmu_cache_if * m_mmu_cache;
 
-  /// pointer to the class with the mmu interface
+  /// Pointer to the class with the mmu interface
   mem_if * m_tlb_adaptor;
 
-  /// cache configuration register (ASI 0x2):
+  /// Cache configuration register (ASI 0x2):
   /// [3]     MMU present - This bit is set to '1' if an MMU is present
   /// [11:4]  Local RAM start address - The 8 MSBs of the local ram start address
   /// [15:12] Local RAM size (LRSZ) - Size in Kbytes of the local scratch pad ram
@@ -184,9 +195,9 @@ class vectorcache : public DefaultBase, public cache_if {
   /// [18:16] Line size (LSIZE) - The size (words) of each cache line
   /// (line size = 2^LSIZE)
   /// [19]    Local RAM (LR) - Set if local scratch pad ram is implemented.
-  /// [23:20] Set size (SSIZE) - Size in Kbytes of each cache set.
-  /// (set size = 2^SSIZE)
-  /// [26:24] Cache associativity (SETS) - Number of sets in the cache
+  /// [23:20] Way size (WSIZE) - Size in Kbytes of each cache way.
+  /// (way size = 2^WSIZE)
+  /// [26:24] Cache associativity (WAYS) - Number of ways in the cache
   /// (000 - direct mapped, 001 - 2-way associative, 010 - 3-way associative, 011 - 4-way associative)
   /// [27]    Cache snooping (SN) - Set if snooping is implemented
   /// [29-28] Cache replacement policiy (REPL)
@@ -194,62 +205,67 @@ class vectorcache : public DefaultBase, public cache_if {
   /// [31]    Cache locking (CL) - Set if cache locking is implemented
   unsigned int CACHE_CONFIG_REG;
 
-  /// the actual cache memory
-  std::vector<t_cache_line> * cache_mem;
+  /// The actual cache memory
+  std::vector<t_cache_line>* cache_mem;
 
-  /// indicates whether the cache can be put in burst mode or not
+  /// Indicates whether the cache can be put in burst mode or not
   unsigned int m_burst_en;
-  /// enables linefetch mode for "newer" as used in "newer" versions
+  /// Enables linefetch mode for "newer" as used in "newer" versions
   /// of dcache in grlib
   bool m_new_linefetch_en;
-  /// pseudo random pointer
+  /// Pseudo-random pointer
   unsigned int m_pseudo_rand;
 
-  // cache parameters
-  // ----------------
-  /// number of cache sets (000 - direct mapped, 001 - 2x, 010 - 3x, 011 -4x
+  /// @} Data
+  /// --------------------------------------------------------------------------
+  /// @name Parameters
+  /// @{
+
+protected:
+  /// Number of cache ways (000: direct mapped, 001: 2x, 010: 3x, 011: 4x)
   unsigned int m_sets;
-  /// indicates size of cacheset in kb = 2^m_setsize
+  /// Size of cache way in kB = 2^m_waysize
   unsigned int m_setsize;
-  /// cache line locking
+  /// Cache line locking
   unsigned int m_setlock;
-  /// indicates size of cacheline in words = 2^m_linesize
+  /// Size of cache line in words = 2^m_linesize
   unsigned int m_linesize;
-  /// number of words per cacheline
+  /// Number of words per cache line
   unsigned int m_wordsperline;
-  /// number of bytes per cacheline
+  /// Number of bytes per cache line
   unsigned int m_bytesperline;
-  /// number of bits for addressing the line offset
+  /// Number of bits for addressing the line offset
   unsigned int m_offset_bits;
-  /// number of lines in the cache
+  /// Number of lines in the cache
   unsigned int m_number_of_vectors;
-  /// address-bits used for index
+  /// Address bits used for index
   unsigned int m_idx_bits;
-  /// address-bits used for tag
+  /// Address bits used for tag
   unsigned int m_tag_bits;
-  /// replacement strategy
+  /// Replacement strategy
   unsigned int m_repl;
 
-  // other parameters
-  // ----------------
-  /// mmu enabled
+  /// MMU enabled
   unsigned int m_mmu_en;
 
-  // lru counter maximum
+  /// LRU counter maximum
   int m_max_lru;
 
   // !!! The actual localram is instantiated in class mmu_cache.
   // !!! Settings are only needed for configuration register.
-
-  /// local ram present
+  /// Local ram present
   unsigned int m_lram;
-  /// start address of localram (8 MSBs)
+  /// Start address of localram (8 MSBs)
   unsigned int m_lramstart;
-  /// size of localram
+  /// Size of localram
   unsigned int m_lramsize;
 
-  // Statistics
-  // ----------
+  /// @} Parameters
+  /// --------------------------------------------------------------------------
+  /// @name Statistics
+  /// @{
+
+protected:
   /// GreenControl API container
   gs::cnf::cnf_api *m_api;
 
@@ -271,12 +287,15 @@ class vectorcache : public DefaultBase, public cache_if {
   /// Counter for bypass operations
   sr_param<uint64_t> bypassops;
 
-  /// enable power monitoring
+  /// Enable power monitoring
   bool m_pow_mon;
 
-  /// *****************************************************
-  /// Power Modeling Parameters
+  /// @} Statistics
+  /// --------------------------------------------------------------------------
+  /// @name Timing and Power Modeling
+  /// @{
 
+protected:
   /// Number of tag ram reads (monitor read & reset)
   sr_param<uint64_t> dyn_tag_reads;
 
@@ -289,8 +308,7 @@ class vectorcache : public DefaultBase, public cache_if {
   /// Number of data ram writes (monitor read & reset)
   sr_param<uint64_t> dyn_data_writes;
 
-  // delay parameters
-  // ----------------
+  /// Timing parameters
   sc_core::sc_time m_hit_read_response_delay;
   sc_core::sc_time m_miss_read_response_delay;
   sc_core::sc_time m_write_response_delay;
@@ -298,6 +316,8 @@ class vectorcache : public DefaultBase, public cache_if {
   /// Clock cycle time
   sc_core::sc_time clockcycle;
 
+  /// @} Timing and Power Modeling
+  /// --------------------------------------------------------------------------
 };
 
 #endif // __VECTORCACHE_H__
