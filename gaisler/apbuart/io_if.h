14c0ce2813235158b1c75e342b980188f7f6b307
Rolf Meyer <rolf.meyer@durania.net>
2016-05-28 21:25:48 +0200

    Changing author of APBUart IO Interface, to be honest the interface was define by Luca Fossati in the RESP project and adaptet to our needs by me, sorry Thomas

--- gaisler/apbuart/io_if.h
+++ gaisler/apbuart/io_if.h
@@ -9,7 +9,7 @@
 ///            Any reproduction, use, distribution or disclosure of this
 ///            program, without the express, prior written consent of the
 ///            authors is strictly prohibited.
-/// @author Thomas Schuster
+/// @author Rolf Meyer
 ///
 
 #ifndef MODELS_APBUART_IO_IF_H_
